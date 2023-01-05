61ced7e8ab8fa4827fc9ca3a08f18f97cc00da40
rynerisraid <120001761@qq.com>
2023-01-05 16:08:19 +0800

    添加菜单

--- dc_in_one/__test__/models/model.spec.js
+++ dc_in_one/__test__/models/model.spec.js
@@ -1,4 +1,5 @@
 const {AuthUser} = require('../../models/auth/AuthUser')
+const { AdminMenu, AdminMenuGroup } = require('../../models/menu/MenuGroup')
 
 /**
  * ORM框架的测试文件
@@ -7,16 +8,37 @@ const {AuthUser} = require('../../models/auth/AuthUser')
 
 it("auth_user",async ()=>{
 
-    const jane = AuthUser.build({ username: "Jane",password:"123456" });
+    // const jane = AuthUser.build({ username: "Jane",password:"123456" });
     
-    await jane.save()
-    // Jane 现在存在于数据库中！
-    console.log(jane instanceof AuthUser); // true
-    console.log(jane.toJSON()); // "Jane"
+    // await jane.save()
+    // // Jane 现在存在于数据库中！
+    // console.log(jane instanceof AuthUser); // true
+    // console.log(jane.toJSON()); // "Jane"
 
+    // 查询所有用户
+    const users = await AuthUser.findAll({attributes: ['username', 'password'],where:{id:1}});
+    //console.log(users[0].dataValues)
+    
+    expect(JSON.stringify(users[0].dataValues)).toBe(JSON.stringify({username: "Jane",password: "123456"}))
 
 })
     
 
+/**
+ * 后台系统的菜单栏
+ * 
+ */
+it("admin_menu", async()=>{
+    // const menu = AdminMenu.build({menu_name:"首页", menu_code:"system_main", parent_id:0,nodetype:0})
+
+    // await menu.save()
+    // // Jane 现在存在于数据库中！
+    // console.log(menu instanceof AdminMenu); // true
+    // console.log(menu.toJSON()); // 
 
 
+    // 查询所有用户
+    const menus = await AdminMenu.findAll();
+    console.log(menus.every(menus => menus instanceof AdminMenu)); // true
+    console.log("All menus:", JSON.stringify(menus, null, 2));
+})
