362e43c86b18b63d25b1f427d84ce6364cde3cf1
rynerisraid <120001761@qq.com>
2023-01-13 15:24:05 +0800

    vuetify

---
+++ vuetify_in_dc/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <Constrained />
+</template>
+
+<script setup>
+//  import HelloWorld from '@/components/HelloWorld.vue'
+import Constrained from '@/layouts/constrained/Constrained.vue'
+</script>
