17c66a50ad9936476cda4f746beff04f02557bb0
adminmttt <mingtlin@foxmail.com>
2023-02-21 23:01:55 +0800

    vye

---
+++ vue-dc/src/utils/lib/LineageGenerator.js
@@ -0,0 +1,32 @@
+//import { antlr, TablePrimary } from "rhombic";
+import {} from 'd'
+
+
+export function parseLineageBySql(input){
+
+  try {
+    const parsingOptions = {
+      // if double quotes should quote identifiers:
+      doubleQuotedIdentifier: true
+    };
+    const q = antlr.parse(input, parsingOptions);
+  
+    console.log('getUsedTables',q.getUsedTables()); // [{ tableName: "abc" }];
+    console.log(q)
+    const getTable = (table) => {
+      /* Logic to retrieve table & columns metadata */
+      console.log(table)
+    };
+  
+    // Whether to use "mergedLeaves" or "tree" lineage type
+    const mergedLeaves = true;
+    const lineageOptions = {
+      positionalRefsEnabled: false
+    };
+    const lineage = q.getLineage(getTable, mergedLeaves, lineageOptions);
+    console.log('lineage',lineage)
+  } catch (e) {
+    // Parsing errors
+  }
+}
+
